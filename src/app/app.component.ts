0e39039b4aebf666ab4bd1faa55da0231b9dcfda
Alex Joel Sajama Calle <asajama@nuevatel.com>
2025-04-21 16:26:40 -0400

    fix tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
 import { SearchComponent } from './shared/components/search/search.component';
 
 @Component({
   selector: 'app-root',
-  imports: [/* RouterOutlet */SearchComponent],
+  imports: [SearchComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'alva_search_view';
