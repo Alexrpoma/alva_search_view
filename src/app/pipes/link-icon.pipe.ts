652d4f9efb168813e4ea43a07459bc18a8f66d6f
Alex Joel Sajama Calle <asajama@nuevatel.com>
2025-04-21 14:33:53 -0400

    ...

--- src/app/pipes/link-icon.pipe.ts
+++ src/app/pipes/link-icon.pipe.ts
@@ -9,11 +9,12 @@ export class LinkIconPipe implements PipeTransform {
     if (!value) return value;
 
     // Regex para detectar URLs
-    const urlRegex = /(https?:\/\/[^\s\]]+)/g;
+    // const urlRegex = /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g;
+    const urlRegex = /(https?:\/\/[^\s\]\)]+)/g;
 
     // Reemplazar cada URL por un ícono con link
     return value.replace(urlRegex, (url) => {
-      return `<a href="${url}" target="_blank" class="btn btn-light btn-sm rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="${url}" style="text-decoration:none;white-space: normal;">
+      return `<a href="${url}" target="_blank" class="btn btn-sm rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="${url}" style="text-decoration:none;white-space: normal; background: #d3e3fd;">
                 <i class="bi bi-link-45deg"></i>
               </a>`;
     });
